f87938e4eb9425603e83ef60be83202d0a782310
Nishant Valvi <nishantvalvi1998@gmail.com>
2024-05-25 14:30:00 +0530

    feat: Add basic page routing and placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import Resume from './pages/resume'
+import Project from './pages/project'
+import Blog from './pages/blog'
 
 function App() {
 
   return (
     <>
-      <div className="">
-      </div>
+        <div className="">
+            <main className="px-4 sm:px-6 py-8 max-w-7xl mx-auto w-full flex-grow ">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/resume" element={<Resume/>}/>
+                    <Route path="/project" element={<Project/>}/>
+                    <Route path="/blog" element={<Blog/>}/>
+                </Routes>
+            </main>
+        </div>
     </>
   )
 }
---
+++ src/pages/resume.tsx
@@ -0,0 +1,9 @@
+const Resume = () => {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Resume Page</h1>
+    </section>
+  );
+};
+
+export default Resume;
